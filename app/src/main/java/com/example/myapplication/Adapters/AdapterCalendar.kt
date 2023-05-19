32ca5966e3d5e6cca2f3dd672e2bbc39896c9f8c
Chifir31 <teppoewa@gmail.com>
2023-05-19 18:46:57 +0300

    тест

--- app/src/main/java/com/example/myapplication/Adapters/AdapterCalendar.kt
+++ app/src/main/java/com/example/myapplication/Adapters/AdapterCalendar.kt
@@ -15,13 +15,15 @@ import kotlin.collections.ArrayList
 
 class AdapterCalendar(val listener: Listener) : RecyclerView.Adapter<AdapterCalendar.CalendarViewHolder>() {
     val weekList = ArrayList<Date>()
+    val statusList = ArrayList<Int>()
     var currentMonday = 0
     class CalendarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val binding = CalendarCellBinding.bind(itemView)
-        fun bind (day: Date, listener: Listener) = with(binding){
+        fun bind (day: Date, listener: Listener, color: Int) = with(binding){
             val date = day.getTime()
-            val dateStr = SimpleDateFormat("dd", Locale("ru")).format(date)
+            val dateStr = SimpleDateFormat("EE\ndd", Locale("ru")).format(date)
             cellDayText.text = dateStr
+            itemView.setBackgroundResource(color)
             itemView.setOnClickListener {
                 listener.onClick(day)
             }
@@ -34,7 +36,17 @@ class AdapterCalendar(val listener: Listener) : RecyclerView.Adapter<AdapterCale
     }
 
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
-        holder.bind(weekList[position], listener)
+        var color = R.color.black
+        if (statusList[position] == 1){
+            color = R.color.teal_200
+        }
+        if (statusList[position] == 0){
+            color = R.color.yellow
+        }
+        if (statusList[position] == -1){
+            color = R.color.red
+        }
+        holder.bind(weekList[position], listener, color)
 
     }
 
@@ -45,6 +57,7 @@ class AdapterCalendar(val listener: Listener) : RecyclerView.Adapter<AdapterCale
 
     fun previousWeekAction() {
         weekList.clear()
+        statusList.clear()
         val now = Calendar.getInstance()
         val weekday = now.get(Calendar.DAY_OF_WEEK)
         val dayOfWeek = 2; // Monday
@@ -56,6 +69,7 @@ class AdapterCalendar(val listener: Listener) : RecyclerView.Adapter<AdapterCale
 
     fun nextWeekAction() {
         weekList.clear()
+        statusList.clear()
         val now = Calendar.getInstance()
         val weekday = now.get(Calendar.DAY_OF_WEEK)
         val dayOfWeek = 2; // Monday
@@ -68,22 +82,47 @@ class AdapterCalendar(val listener: Listener) : RecyclerView.Adapter<AdapterCale
     fun fillWeekList(now: Calendar){
         val dayOfWeek = 2; // Monday
         val weekday = now.get(Calendar.DAY_OF_WEEK)
-        if (weekday !=  1) {
+        if (weekday != 1) {
             val days = dayOfWeek - weekday + currentMonday
             now.add(Calendar.DAY_OF_YEAR, days)
         } else{
             val days = -6 + currentMonday
             now.add(Calendar.DAY_OF_YEAR, days)
         }
-        val date = now.getTime()
+        var date = now.getTime()
+
+        Log.d("FFF", now.get(Calendar.DAY_OF_WEEK).toString() )
+        Log.d("FFF", weekday.toString())
+        if (now.get(Calendar.DAY_OF_WEEK) == weekday){
+            fillStatusList()
+        } else {
+            statusList.add((-1..1).random())
+        }
+
         weekList.add(date)
+
         (1..6).forEach {
             now.add(Calendar.DAY_OF_YEAR, +1)
-            val date = now.getTime()
+            if (now.get(Calendar.DAY_OF_WEEK) == weekday){
+                fillStatusList()
+            } else {
+                if (statusList.size != 7) {
+                    statusList.add((-1..1).random())
+                }
+            }
+            date = now.getTime()
             weekList.add(date)
         }
+        Log.d("statusList:", statusList.toString())
         notifyDataSetChanged()
     }
+
+    fun fillStatusList(){
+        while (statusList.size != 7){
+            statusList.add(2)
+        }
+    }
+
 
     interface Listener{
         fun onClick(day: Date)
---
+++ app/src/main/java/com/example/myapplication/data/CalendarStatus.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.data
+
+import java.util.Date
+
+data class CalendarStatus(var day: Date, var status: Int)
